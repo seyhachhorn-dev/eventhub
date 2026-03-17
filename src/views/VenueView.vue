75d7b4ebb589c5b811622fed7cb57b2a3b5c0ce1
CHHORN Seyha <senjihachiro@gmail.com>
2026-03-17 10:32:19 +0700

    feat:add all page title and button

--- src/views/VenueView.vue
+++ src/views/VenueView.vue
@@ -1,5 +1,15 @@
 <template>
-    <main>
-        <h1>Hello Venue</h1>
-    </main>
+  <div class="p-8">
+    <div class="flex items-center justify-between">
+      <div>
+        <h2 class="text-4xl font-bold">Venue</h2>
+        <p class="text-gray-500 mt-2">Manage your venues and registrations</p>
+      </div>
+      <Button variant="outline">+ Create Venue</Button>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
